c47155722a83f1c60661501c1298ca00d4c535a9
Max Leduc <leduc.maximep@gmail.com>
2016-05-26 16:45:27 -0400

    Ajax call and array building

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,3 +1,56 @@
-$(function() {
-	alert("Hello, Run-DMD!");
+"use strict";
+
+const beausBeers = {
+
+};
+
+beausBeers.apiKey = 'MDpiMzJiZTJiYy0yMzU4LTExZTYtYjc2YS1lYjM1ZTNhY2NmN2U6Rk5DQ2ZjQUJlRElHVnM5YTBhWFBUNlQwWWhQR0RSSW9ydENF';
+// console.log(beausBeers.apiKey);
+
+beausBeers.beerArray = [];
+
+beausBeers.init = function(){
+	beausBeers.getData();
+};
+
+beausBeers.getData = function(){
+	// console.log('this is working');
+	$.ajax({
+		url:'https://lcboapi.com/products',
+		headers: { 'Authorization': ' Token ' + beausBeers.apiKey},
+		data: {
+			q:"Beau's",
+			where: 'is_seasonal',
+			where_not: 'is_dead',
+			where_not: 'is_discontinued'
+		}
+	})
+	.then(function(lcboObjects){
+		console.log(lcboObjects);
+		let beerResult = lcboObjects.result;
+		beausBeers.dataArray(beerResult);
+	});
+};
+
+beausBeers.dataArray = function(beerResult){
+	for(let i = 0; i < beerResult.length; i++){
+		beausBeers.beerArray[i] = {
+			name: beerResult[i].name,
+			style: beerResult[i].style,
+			price: '$' + (beerResult[i].price_in_cents/100),
+			alcohol_content: (beerResult[i].alcohol_content/100) + '%',
+			volume: beerResult[i].volume_in_milliliters + ' ml' + ' ' + beerResult[i].package_unit_type,
+			secondary_category: beerResult[i].secondary_category,
+			tertiary_category:beerResult[i].tertiary_category,
+		};
+		// beausBeers.beerArray[i].push(beerResult[i].name);
+
+	}
+	// console.log('Working');
+	console.log(beausBeers.beerArray);
+
+};
+
+$(document).ready(function(){
+	beausBeers.init();
 });
